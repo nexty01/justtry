03e2211a531c22a555b5ca53d8b18d959d5b49ad
nexty01 <fakejeson@gmail.com>
2025-08-24 17:39:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname));
-
+// Serve friendship.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "bestfriend.html"));
+  res.sendFile(path.join(__dirname, "friendship.html"));
 });
 
+// Heroku assigns PORT automatically
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🎁 App running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
