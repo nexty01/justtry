6b48d53ef41667cd9ca2dea6f2d3c1005495a94e
nexty01 <fakejeson@gmail.com>
2025-08-24 14:31:32 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "bestfriend.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🎁 App running on http://localhost:${PORT}`));
