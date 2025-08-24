d0bccf32587cd8afedf9370ffcb7ea962b6c7d73
nexty01 <fakejeson@gmail.com>
2025-08-24 21:16:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Public folder ko static serve karo
 app.use(express.static(path.join(__dirname, "public")));
 
-// Root pe friendship.html serve karo
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "friendship.html"));
 });
 
-// Server start
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
